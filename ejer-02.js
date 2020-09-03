10de78a9f312aec31c63df8823e56affbe85ab5a
fragata-adrian <fragataadrian99@gmail.com>
2020-09-03 17:32:16 -0300

    first commit

---
+++ ejer-02.js
@@ -0,0 +1,66 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('clase04', 'root', 'root', {
+    host: 'localhost',
+    dialect: 'mariadb'
+});
+
+sequelize
+    .authenticate()
+    .then(() => { 
+        console.log('Coneccion establecida exitosamente.');
+    })
+    .catch(err => {
+        console.error('No se pudo conectar a la base de datos.', err)
+    });
+
+const Model = Sequelize.Model;
+
+class Usuario extends Model {}
+Usuario.init({
+    nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    apellido: {
+        type: Sequelize.STRING
+    }
+}, {
+    sequelize,
+    modelName: 'usuario'
+});
+
+sequelize.sync()
+    .then(() => Usuario.create({
+        nombre: 'Juan',
+        apellido: 'Gonzales'
+    }))
+    .then(jane => { 
+        console.log(jane.toJSON()); 
+});
+
+sequelize.sync()
+    .then(() => Usuario.create({
+        nombre: 'Sofia',
+        apellido: 'Perez'
+    }))
+    .then(jane => { 
+        console.log(jane.toJSON()); 
+});
+
+sequelize.sync()
+    .then(() => Usuario.create({
+        nombre: 'Pepe',
+        apellido: 'Caceres'
+    }))
+    .then(jane => { 
+        console.log(jane.toJSON()); 
+});
+
+Usuario.destroy({ 
+    where: {
+        apellido: 'Perez'
+    } 
+}).then(() => {
+    console.log("Registro Eliminado.");
+})
